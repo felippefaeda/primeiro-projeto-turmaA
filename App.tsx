e98d2f91f42fb3e5bdd985b8849dd70f6cacd61c
Felippe Faeda <felippefaeda@gmail.com>
2022-05-13 15:23:51 -0300

    Alterção no texto da página inicial

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Boa tarde</Text>
+      <Text>Boa Noite</Text>
       <StatusBar style="auto" />
     </View>
   );
